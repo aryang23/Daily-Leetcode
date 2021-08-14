c0d5caf0da32a87041ad20df0c4e36b68bfc19a6
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-08-14 10:57:34 +0530

    Update 695.Max_Area_of_Island.java

--- 695.Max_Area_of_Island.java
+++ 695.Max_Area_of_Island.java
@@ -34,3 +34,48 @@ class Solution {
         return count;
     }
 }
+
+
+
+
+
+
+
+//M-2
+
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int n = grid.length, m = grid[0].length, maxSize = 0;
+        int[][] dir = {{1,0},{-1,0},{0,-1},{0,1}};
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    int s = maxAreaOfIslandHelper(grid, dir, i, j);
+                    maxSize = Math.max(maxSize, s);
+                }
+            }
+        }
+        return maxSize;
+    }
+    
+    public int maxAreaOfIslandHelper(int[][] grid, int[][] dir, int sr, int sc)
+    {
+        grid[sr][sc] = 0;
+        int size = 0;
+        for(int d=0;d<4;d++)
+        {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            
+            if(r>=0 && c>=0 && r<grid.length && c<grid[0].length &&  grid[r][c]==1)
+                size += maxAreaOfIslandHelper(grid, dir, r, c);
+        }
+        
+        return size + 1;
+    }
+}
