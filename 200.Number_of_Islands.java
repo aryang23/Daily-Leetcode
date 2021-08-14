0ca42f1ca79d4a4ec7c8f508cefe58a64a01b0c9
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-08-14 10:48:37 +0530

    Update 200.Number_of_Islands.java

--- 200.Number_of_Islands.java
+++ 200.Number_of_Islands.java
@@ -29,3 +29,46 @@ class Solution {
         numIslandsHelper(grid, visited, i, j-1);
     }
 }
+
+
+
+
+
+
+
+
+
+
+//Soln2: -
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length, m = grid[0].length, componentCount = 0;
+        int[][] dir = {{1,0},{-1,0},{0,-1},{0,1}};
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    dfs(grid, dir, i, j);
+                    componentCount++;
+                }
+            }
+        }
+        return componentCount;
+    }
+    
+    public void dfs(char[][] grid, int[][] dir, int sr, int sc)
+    {
+        grid[sr][sc] = '0';
+        for(int d=0;d<4;d++)
+        {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            
+            if(r>=0 && c>=0 && r<grid.length && c<grid[0].length && grid[r][c]=='1')
+                dfs(grid, dir, r, c);
+        }
+    }
+}
