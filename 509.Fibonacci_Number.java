07322dd89eddb358acec5227767fe19cd9f3d233
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-08-30 18:09:39 +0530

    Update 509.Fibonacci_Number.java

--- 509.Fibonacci_Number.java
+++ 509.Fibonacci_Number.java
@@ -3,12 +3,12 @@ class Solution {
         if(n==0)
             return 0;
         int a = 0, b = 1, c = 1;
-        for(int i=0;i<n-1;i++)
+        for(int i=2;i<=n;i++)
         {
             c=a+b;
             a=b;
             b=c;
         }
-        return c;
+        return b;
     }
 }
