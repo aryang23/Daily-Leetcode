cde65fc46fb4458385b4234409bd3a186f1b1c0f
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-11 01:32:47 +0530

    Create 131.Palindrome_Partitioning.java

---
+++ 131.Palindrome_Partitioning.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        func(0, s, path, res);
+        return res;
+    }
+    public void func(int index, String s, List<String> path, List<List<String>> res)
+    {
+        if(index==s.length())
+        {
+            List<String> ba = new ArrayList<>(path);
+            res.add(ba);
+            return;
+        }
+        for(int i=index;i<s.length();i++)
+        {
+            if(isPalindrome(s, index, i))
+            {
+                path.add(s.substring(index, i+1));
+                func(i+1, s, path, res);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+    
+    public boolean isPalindrome(String s, int start, int end)
+    {
+        while(start <= end)
+        {
+            if(s.charAt(start++) != s.charAt(end--))
+                return false;
+        }
+        return true;
+    }
+}
