f0a8df86d45974368cdd25a73695be46ccb800da
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-14 18:44:39 +0530

    Create 673.Number_of_Longest_Increasing_Subsequence.java

---
+++ 673.Number_of_Longest_Increasing_Subsequence.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findNumberOfLIS(int[] arr) {
+        int n = arr.length, maxLen = 0, maxCount = 0;
+        int[] dp = new int[n];
+        int[] count = new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            dp[i] = 1;
+            count[i] = 1;
+            for(int j=i-1;j>=0;j--)
+            {
+                if(arr[i] > arr[j])
+                {
+                    if(dp[j]+1 > dp[i])
+                    {
+                        dp[i] = dp[j]+1;
+                        count[i] = count[j];
+                    }
+                    else if(dp[i] == dp[j]+1)
+                    {
+                        count[i] += count[j];
+                    }
+                }
+            }
+            if(dp[i] > maxLen)
+            {
+                maxLen = dp[i];
+                maxCount = count[i];
+            }
+            else if(dp[i] == maxLen)
+            {
+                maxCount += count[i];
+            }
+        }
+        return maxCount;
+    }
+}
