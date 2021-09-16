884f0614a2e43716045a9f17e2564a6863344f36
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-16 20:11:35 +0530

    Create 322.Coin_Change.java

---
+++ 322.Coin_Change.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int coinChange(int[] arr, int Tar) {
+        int[] dp = new int[Tar+1];
+        Arrays.fill(dp, (int)1e9);
+        dp[0] = 0;
+        for(int tar=1;tar<=Tar;tar++)
+        {
+            for(int ele:arr)
+            {
+                if(tar-ele >= 0)
+                    dp[tar] = Math.min(dp[tar], dp[tar-ele]+1);
+            }
+        }
+        return dp[Tar]!=(int)1e9 ? dp[Tar] : -1;
+    }
+}
