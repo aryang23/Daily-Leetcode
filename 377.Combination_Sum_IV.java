9458c1f70d4f403d2d388cdb90406fe28b83d84a
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-17 23:43:49 +0530

    Update 377.Combination_Sum_IV.java

--- 377.Combination_Sum_IV.java
+++ 377.Combination_Sum_IV.java
@@ -2,9 +2,27 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target+1];
         Arrays.fill(dp, -1);
-        return combinationSum4_tabu(nums, target, dp);
+        int ans = comb4_memo(nums, target, dp);
+        // int ans = comb4_tabu(nums, target, dp);
+        return ans;
     }
-    public int combinationSum4_tabu(int[] arr, int TARGET, int[] dp)
+    
+    public int comb4_memo(int[] arr, int target, int[] dp)
+    {
+        if(target==0)
+            return dp[target] = 1;
+        if(dp[target]!=-1)
+            return dp[target];
+        
+        int count = 0;
+        for(int ele:arr)
+        {
+            if(target-ele>=0)
+                count += comb4_memo(arr, target-ele, dp);
+        }
+        return dp[target] = count;
+    }
+    public int comb4_tabu(int[] arr, int TARGET, int[] dp)
     {
         for(int target=0;target<=TARGET;target++)
         {
@@ -13,12 +31,12 @@ class Solution {
                 dp[target] = 1;
                 continue;
             }
-            
+
             int count = 0;
             for(int ele:arr)
             {
-                if(target-ele >= 0)
-                    count += dp[target-ele]; 
+                if(target-ele>=0)
+                    count += dp[target-ele];
             }
             dp[target] = count;
         }
