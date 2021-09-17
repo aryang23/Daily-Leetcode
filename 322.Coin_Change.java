cc46f91869c8d3c867310ccc364f520e60100982
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-18 00:12:18 +0530

    Update 322.Coin_Change.java

--- 322.Coin_Change.java
+++ 322.Coin_Change.java
@@ -1,8 +1,8 @@
 class Solution {
     public int coinChange(int[] arr, int Tar) {
         int[] dp = new int[Tar+1];
-        Arrays.fill(dp, (int)1e9);
-        dp[0] = 0;
+        Arrays.fill(dp, (int) 1e9);
+        dp[0] =0;
         for(int tar=1;tar<=Tar;tar++)
         {
             for(int ele:arr)
