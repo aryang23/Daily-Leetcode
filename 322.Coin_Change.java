b4e65a990af4df76656b845489b33715fa09d027
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-18 10:13:14 +0530

    Update 322.Coin_Change.java

--- 322.Coin_Change.java
+++ 322.Coin_Change.java
@@ -1,8 +1,9 @@
 class Solution {
-    public int coinChange(int[] arr, int Tar) {
+    //Permutation Code
+    public int coinChange2(int[] arr, int Tar) {
         int[] dp = new int[Tar+1];
         Arrays.fill(dp, (int) 1e9);
-        dp[0] =0;
+        dp[0] = 0;
         for(int tar=1;tar<=Tar;tar++)
         {
             for(int ele:arr)
@@ -12,5 +13,22 @@ class Solution {
             }
         }
         return dp[Tar]!=(int)1e9 ? dp[Tar] : -1;
+    }
+    
+    //Combination Code
+    public int coinChange(int[] arr, int Tar)
+    {
+        int[] dp = new int[Tar+1];
+        Arrays.fill(dp, (int)1e9);
+        dp[0] = 0;
+        for(int ele:arr)
+        {
+            for(int tar=ele;tar<=Tar;tar++)
+            {
+                if(tar-ele >= 0)
+                    dp[tar] = Math.min(dp[tar], dp[tar-ele]+1);
+            }
+        }
+        return dp[Tar]==(int)1e9?-1:dp[Tar];
     }
 }
