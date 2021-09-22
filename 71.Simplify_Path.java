bd023586e04cc2b90f796dd85efd9f612be26f3d
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-22 15:20:06 +0530

    Create 71.Simplify_Path.java

---
+++ 71.Simplify_Path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] tokens = path.split("/+");
+        Stack<String> stack = new Stack<>();
+        for(String t:tokens)
+        {
+            if(stack.size()>0 && "..".equals(t))
+            {
+                stack.pop();
+            }
+            else  if(!Arrays.asList("",".","..").contains(t))
+                stack.push(t);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(String s:stack)
+        {
+            sb.append("/").append(s);
+        }
+        
+        return sb.length()== 0 ? "/" : sb.toString();
+    }
+}
