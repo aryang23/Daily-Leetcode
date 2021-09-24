25dbd835f94fd0d6677021ab477f636f2677cb2d
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-24 18:10:47 +0530

    Create 1492.The_kth_Factor_of_n.java

---
+++ 1492.The_kth_Factor_of_n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        int j=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                j++;
+                if(j==k)
+                    return i;
+            }
+        }
+        return -1;
+    }
+}
