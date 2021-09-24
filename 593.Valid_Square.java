86a140074717fea33b63d01a601e4d48c2e164b9
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2021-09-24 15:27:07 +0530

    Create 593.Valid_Square.java

---
+++ 593.Valid_Square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        HashSet<Integer> set = new HashSet<>(Arrays.asList(distance(p1, p2), distance(p1, p3), distance(p1, p4), distance(p2, p3), distance(p2, p4), distance(p3, p4)));
+        return !set.contains(0) && set.size()==2;
+    }
+    
+    private int distance(int[] a, int[] b)
+    {
+        return (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
+    }
+}
