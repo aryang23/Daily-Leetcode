cd5afb56c11c56404e10dacb66d6cb3c74f2460d
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-02 15:33:28 +0530

    Create 2125. Number of Laser Beams in a Bank.java

---
+++ 2125. Number of Laser Beams in a Bank.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int pSecurityDevicesFreq = 0;
+        int ans = 0;
+        for(String s:bank)
+        {
+            int cSecurityDevicesFreq = 0;
+            for(char c:s.toCharArray())
+            {
+                if(c=='1')
+                {
+                    cSecurityDevicesFreq++;
+                }
+            }
+            
+            if(cSecurityDevicesFreq > 0)
+            {
+                ans += cSecurityDevicesFreq * pSecurityDevicesFreq;
+                pSecurityDevicesFreq = cSecurityDevicesFreq;
+            }
+        }
+        return ans;
+    }
+}
