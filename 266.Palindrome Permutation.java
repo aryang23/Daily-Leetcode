62eec70187d1d36afc2a3abf4a9b7f51f6c2b402
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-04 01:12:42 +0530

    Create 266.Palindrome Permutation.java

---
+++ 266.Palindrome Permutation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        int count = 0;
+        for(char i=0;i<128 && count<=1;i++)
+        {
+            int ct = 0;
+            for(int j=0;j<s.length();j++)
+            {
+                if(s.charAt(j)==i)
+                    ct++;
+            }
+            count += ct % 2;
+        }
+        return count <= 1;
+    }
+}
