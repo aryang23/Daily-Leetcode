998b8bed24143fc63d1edda5c1cb8cae045344a8
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-15 15:46:42 +0530

    Time: 1 ms (69.51%), Space: 38.5 MB (30.72%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int splitArray(int[] pages, int m) {
+        int n = pages.length;
+        if(m>n)
+        {
+            return -1;
+        }
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int val : pages){
+            sum += val;
+            max = Math.max(max, val);
+        }
+        
+        int lo = max;
+        int hi = sum;
+        
+        int burden = Integer.MAX_VALUE;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(isBurdenPossible(pages, mid, m) == true){
+                burden = mid;
+                hi = mid-1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return burden;
+    }
+    private static boolean isBurdenPossible(int[] pages, int burden, int m) {
+        int student = 1;
+        int sum = 0;
+        
+        for(int i=0;i<pages.length;i++) {
+            sum += pages[i];
+            if(sum > burden) {
+                student++;
+                sum = pages[i];
+            }
+            
+        }
+        return student <= m;
+    }
+}
