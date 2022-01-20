1aa41d5d12f9e586fd10a88a8a1c16d43dc73d35
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-20 11:19:58 +0530

    Time: 1 ms (98.94%), Space: 39 MB (51.58%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+                int ans = 0;
+        int temp = n;
+        int idx =0;
+        while(temp!=0)
+        {
+            int sb = temp&1;
+            temp>>>=1;
+            ans=ans|((sb)<<(31-idx));
+            idx++;
+        }
+        return ans;
+
+    }
+}
