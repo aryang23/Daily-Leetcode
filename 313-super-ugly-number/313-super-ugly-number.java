c477417bbddf0a81ee325209ce25e58388ff317f
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-24 00:05:15 +0530

    Time: 415 ms (63.39%), Space: 43.1 MB (28.45%) - LeetHub

---
+++ 313-super-ugly-number/313-super-ugly-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] dp = new int[n+1];
+    dp[1] = 1;
+    
+    int[] pointers = new int[primes.length];
+    Arrays.fill(pointers, 1);
+    
+    for(int i=2; i<=n; i++) {
+        int min = Integer.MAX_VALUE;
+        for(int j=0; j<primes.length; j++) {
+            min = Math.min(min, primes[j]*dp[pointers[j]]);
+        }
+        dp[i] = min;
+        
+        for(int j=0; j<primes.length; j++) {
+            if(primes[j] * dp[pointers[j]] == min) {
+                pointers[j]++;
+            }
+        }
+    }
+    return dp[n];
+    }
+}
