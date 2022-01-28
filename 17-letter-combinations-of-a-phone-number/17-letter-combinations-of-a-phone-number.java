9b2c226f0014ee79996c64d0e464a51ebcdd2f7d
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-28 19:21:30 +0530

    Time: 9 ms (16.85%), Space: 43.4 MB (6.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -3,7 +3,8 @@ class Solution {
         List<String> ans = new ArrayList<>();
         if(digits.length() == 0)
             return ans;
-        ans = kpc(digits);
+        // ans = kpc(digits);
+        ans = kpc2(digits, 0);
         return ans;
     }
     
@@ -19,6 +20,32 @@ class Solution {
         int num = str.charAt(0) - '0';
         String ls = str.substring(1);
         List<String> smallAns = kpc(ls);
+        
+        String smallStr = arr[num-1];
+        
+        List<String> ans = new ArrayList<>();
+        
+        for(int i=0; i<smallStr.length(); i++) {
+            char ch = smallStr.charAt(i);
+            for(int j=0; j<smallAns.size(); j++) {
+                ans.add(ch + smallAns.get(j));
+            }
+        }
+        return ans;
+    }
+    
+    public List<String> kpc2(String str, int idx) {
+        String[] arr = {".;","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
+        if(idx == str.length()) {
+            ArrayList<String> ba = new ArrayList<>();
+            ba.add("");
+            return ba;
+        }
+        
+        int num = str.charAt(idx) - '0';
+        // String ls = str.substring(1);
+        List<String> smallAns = kpc2(str, idx+1);
         
         String smallStr = arr[num-1];
         
