f523b7ee7c17df3eea06de077c8ab15d58fb89a6
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-28 17:49:12 +0530

    Time: 8 ms (19.47%), Space: 42.6 MB (6.43%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if(digits.length() == 0)
+            return ans;
+        ans = kpc(digits);
+        return ans;
+    }
+    
+    public List<String> kpc(String str) {
+        String[] arr = {".;","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
+        if(str.length() == 0) {
+            ArrayList<String> ba = new ArrayList<>();
+            ba.add("");
+            return ba;
+        }
+        
+        int num = str.charAt(0) - '0';
+        String ls = str.substring(1);
+        List<String> smallAns = kpc(ls);
+        
+        String smallStr = arr[num-1];
+        
+        List<String> ans = new ArrayList<>();
+        
+        for(int i=0; i<smallStr.length(); i++) {
+            char ch = smallStr.charAt(i);
+            for(int j=0; j<smallAns.size(); j++) {
+                ans.add(ch + smallAns.get(j));
+            }
+        }
+        return ans;
+    }
+}
