b50f8a6020f49b04bd8c752b4d91fccb5f1b8ae6
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-31 09:43:07 +0530

    Time: 1 ms (99.99%), Space: 42.4 MB (14.28%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> sa = new ArrayList<>();
+        hasPath(root, targetSum, 0, sa, res);
+        return res;
+    }
+//     public void hasPath(TreeNode root, int target, int sum, List<Integer> sa, List<List<Integer>> res) {
+//         if(root==null)
+//             return;
+        
+//         if(root.left==null && root.right==null){
+//             sum += root.val;
+//             // System.out.println(sum);
+//             sa.add(root.val);
+//             if(sum==target)
+//                 res.add(sa);
+//             //sa.remove(sa.size()-1);
+//             return;
+//         }
+        
+//         sa.add(root.val);
+//         hasPath(root.left, target, sum+root.val, sa, res);
+//         hasPath(root.right, target, sum+root.val, sa, res);       
+//         sa.remove(sa.size()-1);
+        
+        
+//     }
+    
+    public void hasPath(TreeNode root, int target, int sum, List<Integer> sa, List<List<Integer>> res) {
+        if(root==null)
+            return;
+        
+        if(root.left==null && root.right==null) {
+            sum += root.val;
+            if(sum==target) {
+                sa.add(root.val);
+                List<Integer> ba = new ArrayList<>(sa);
+                res.add(ba);
+                sa.remove(sa.size()-1);
+            }
+            return;
+        }
+        sa.add(root.val);
+        hasPath(root.left, target, sum + root.val, sa, res);
+        hasPath(root.right, target, sum + root.val, sa, res);
+        sa.remove(sa.size()-1);
+    }
+}
