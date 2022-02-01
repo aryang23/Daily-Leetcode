e9c3257ae423ada0224217ef6d0664d14865ad16
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-01 15:56:29 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (54.55%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null)
+            return null;
+        if(root.val == val)
+            return root;
+        TreeNode ans = null;
+        if(root.val < val) {
+            ans = searchBST(root.right, val);
+        } else {
+            ans = searchBST(root.left, val);
+        }
+        return ans;
+    }
+}
