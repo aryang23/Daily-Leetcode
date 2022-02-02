6940318977111dfa3b3dca4f86d42646f451c287
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-02 12:29:59 +0530

    Time: 14 ms (66.43%), Space: 47.6 MB (18.13%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        
+        int[] hash = new int[26];
+        int[] phash = new int[26];
+        
+        int window = p.length();
+        int len = s.length();
+        
+        if(len < window)
+            return ans;
+        
+        int left=0, right=0;
+        
+        while(right<window)
+        {
+            phash[p.charAt(right)-'a']+=1;
+            hash[s.charAt(right++)-'a']+=1;
+        }
+        
+        right -= 1;
+        while(right < len)
+        {
+            // display(hash);
+            // display(phash);
+            if(areEqual(hash, phash))
+                ans.add(left);
+            right += 1;
+            if(right != len)
+                hash[s.charAt(right)-'a'] += 1;
+            hash[s.charAt(left)-'a'] -= 1;
+            left += 1;
+        }
+        return ans;
+    }
+    
+    public static boolean areEqual(int[] arr1, int[] arr2)
+    {
+        for(int i=0;i<arr1.length;i++)
+        {
+            if(arr1[i]!=arr2[i])
+                return false;
+        }
+        return true;
+    }
+    public static void display(int[] dp) {
+        for (int ele : dp) 
+        {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+}
