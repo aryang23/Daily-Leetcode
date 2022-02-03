d85b867e2169aa3d7383994130ef436e06f25f6e
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-03 23:57:44 +0530

    Time: 23 ms (47.43%), Space: 43.6 MB (39.88%) - LeetHub

--- 87-scramble-string/87-scramble-string.java
+++ 87-scramble-string/87-scramble-string.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isScramble(String s1, String s2) {
         int n = s1.length();
-		boolean ans = isScramble(s1,s2, 0, 0, s1.length(), new Boolean[n][n][n+1]);
+		// boolean ans = isScramble(s1,s2, 0, 0, s1.length(), new Boolean[n][n][n+1]);
+        boolean ans = isScrambleTabu(s1, s2);
         return ans;
     }
     
@@ -23,5 +24,33 @@ class Solution {
 	    
 	    dp[si1][si2][len] = false;
 	    return false;
+	}
+    
+    public static boolean isScrambleTabu(String s1, String s2) {
+	    if(s1.equals(s2) == true) {
+	        return true;
+	    }
+	    int n = s1.length();
+	    boolean[][][] dp = new boolean[n][n][n+1];
+	    
+	    for(int len=1; len<=n; len++) {
+	        for(int i=0; i<=n-len; i++) {
+	            for(int j=0; j<=n-len; j++) {
+	                if(len==1) {
+	                    if(s1.charAt(i) == s2.charAt(j)) {
+	                        dp[i][j][1] = true;
+	                    }
+	                }
+	                 else {
+	                        for(int cp=1; cp<len; cp++) {
+	                            dp[i][j][len] = ((dp[i][j][cp] && dp[i+cp][j+cp][len-cp]) || (dp[i][j+len-cp][cp] && dp[i+cp][j][len-cp]));
+	                            if(dp[i][j][len] == true)
+	                            break;
+	                        }
+	                    }
+	            }
+	        }
+	    }
+	    return dp[0][0][n];
 	}
 }
