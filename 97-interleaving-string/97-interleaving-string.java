e547d7c20fddaaeb511e9e119583dbb504bb7dba
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-08 00:22:04 +0530

    Time: 8 ms (35.03%), Space: 42.4 MB (15.15%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n = s1.length();
+        int m = s2.length();
+        if(n+m!=s3.length())
+            return false;
+        boolean[][] dp = new boolean[n+1][m+1];
+        // dp[0][0] = 1;
+
+        for(int i=0; i<=n; i++) {
+            for(int j=0; j<=m; j++) {
+                if(i==0 && j==0) {
+                    dp[i][j] = true;
+                } else if(i==0) {
+                    dp[i][j] = s2.charAt(j-1)==s3.charAt(i+j-1)?dp[i][j-1]:false;
+                } else if(j==0) {
+                    dp[i][j] = s1.charAt(i-1)==s3.charAt(i+j-1)?dp[i-1][j]:false;
+                } else {
+                    char cins1 = s1.charAt(i-1);
+                    char cins2 = s2.charAt(j-1);
+                    char cins3 = s3.charAt(i+j-1);
+
+                    if(cins1==cins3 && cins2 == cins3) {
+                        dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    } else if(cins1==cins3) {
+                        dp[i][j] = dp[i-1][j];
+                    } else if(cins2 == cins3) {
+                        dp[i][j] = dp[i][j-1];
+                    } else {
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
