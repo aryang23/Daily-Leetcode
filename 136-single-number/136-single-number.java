f7ef46d3dc40bfae8d025221fd4ab99904c88476
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-15 14:06:08 +0530

    Time: 1 ms (95.92%), Space: 50.5 MB (30.87%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,10 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
-        for(int i=0;i<nums.length;i++)
-        {
-            ans=ans^nums[i];
-        }
+        for(int val:nums)
+            ans ^= val;
         return ans;
     }
 }
