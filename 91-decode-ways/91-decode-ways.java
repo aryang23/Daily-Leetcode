a1d1f893040cf08dd08197562634562e2421d9f3
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-21 09:49:34 +0530

    Time: 1 ms (95.44%), Space: 42.4 MB (18.84%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -5,7 +5,8 @@ class Solution {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
         // int ans = numDecodingsHelper(s);
-        int ans = countEncodings(s);
+        // int ans = countEncodings(s);
+        int ans = numDecodings(s, 0, dp);
         return ans;
    }
     public int numDecodingsHelper(String str) {
@@ -62,5 +63,28 @@ class Solution {
             }
         }
         return dp[n];
+    }
+    
+    public int numDecodings(String s, int idx, int[] dp) {
+        if(idx == s.length()) {
+            return dp[idx] = 1;
+        }
+        if(dp[idx] != -1) {
+            return dp[idx];
+        }
+        
+        char ch = s.charAt(idx);
+        if(ch=='0') {
+            return dp[idx] = 0;
+        }
+        int count = numDecodings(s, idx+1, dp);
+        if(idx < s.length() - 1) {
+            char ch1 = s.charAt(idx+1);
+            int num = (ch-'0')*10 +(ch1-'0');
+            if(num <= 26) {
+                count += numDecodings(s, idx+2, dp);
+            }
+        }
+        return dp[idx] = count;
     }
 }
