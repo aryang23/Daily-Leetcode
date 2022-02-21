93c841fbdc87902aa696518af83549333e2b62cd
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-21 10:39:49 +0530

    Time: 1 ms (99.71%), Space: 40.5 MB (66.09%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        int len = 0;
+        int n = s.length();
+        for(String ss:wordDict) {
+            set.add(ss);
+            len = Math.max(ss.length(), len);
+        }
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        for(int i=0; i<=n; i++) {
+            if(!dp[i]) {
+                continue;
+            }
+            
+            for(int l=1; l<=len && l+i<=n; l++) {
+                String substr = s.substring(i, i+l);
+                if(set.contains(substr) == true) {
+                    dp[i+l] = true;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
