b841dfab5bc14ca5fb6a6222a11e4fd2ade9ab34
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-02-24 18:59:43 +0530

    Time: 7 ms (18.48%), Space: 43.4 MB (5.87%) - LeetHub

---
+++ 781-rabbits-in-forest/781-rabbits-in-forest.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRabbits(int[] answers) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<answers.length; i++) {
+            int nf = map.getOrDefault(answers[i], 0)+1;
+            map.put(answers[i], nf);
+        }
+        
+        int mr = 0;
+        for(int key:map.keySet()) {
+            int val = map.get(key);
+            int gs = key + 1;
+            mr += Math.ceil(val*1.0/gs)*gs;
+        }
+        return mr;
+    }
+}
