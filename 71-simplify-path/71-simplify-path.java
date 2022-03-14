422f9ca1461db098ed729ad2b2b1b8058545874e
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-03-14 10:48:14 +0530

    Time: 14 ms (20.01%), Space: 44.9 MB (12.24%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -2,19 +2,17 @@ class Solution {
     public String simplifyPath(String path) {
         String[] tokens = path.split("/+");
         Stack<String> stack = new Stack<>();
-        for(String t:tokens)
-        {
-            if(stack.size()>0 && "..".equals(t))
-            {
+        
+        for(String t:tokens) {
+            if(stack.size()>0 && "..".equals(t)) {
                 stack.pop();
-            }
-            else  if(!Arrays.asList("",".","..").contains(t))
+            } else  if(!Arrays.asList("",".","..").contains(t)) {
                 stack.push(t);
+            }
         }
         
         StringBuilder sb = new StringBuilder();
-        for(String s:stack)
-        {
+        for(String s:stack) {
             sb.append("/").append(s);
         }
         
