9b9f0ed289287efcc1fd8305d61975dcf82ef32a
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-04-07 08:43:47 +0530

    Time: 4 ms (12.15%), Space: 41.4 MB (61.79%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        List<Integer> stoneList = new ArrayList<>();
+        for(int weight : stones)
+            stoneList.add(weight);
+        
+        while(stoneList.size() > 1)
+        {
+            int stone1 = removeLargest(stoneList);
+            int stone2 = removeLargest(stoneList);
+            if(stone1 != stone2)
+                stoneList.add(stone1 - stone2);
+        }
+        
+        return stoneList.isEmpty()?0:stoneList.remove(0);
+    }
+    
+    public int removeLargest(List<Integer> stones)
+    {
+        int idxOfLargest = stones.indexOf(Collections.max(stones));
+        int result = stones.get(idxOfLargest);
+        stones.set(idxOfLargest, stones.get(stones.size() - 1));
+        stones.remove(stones.size() - 1);
+        return result+2;
+    }
+}
