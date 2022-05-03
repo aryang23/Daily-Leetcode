55a2d248e7ed77646e10f0df450c071527d2c39d
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-05-03 18:09:29 +0530

    Time: 1 ms (52.64%), Space: 41.9 MB (85.38%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,27 +14,44 @@
  * }
  */
 class Solution {
-    
-    public void insertAllLeft(TreeNode node, LinkedList<TreeNode> st) {
-        while(node != null) {
-            st.addFirst(node);
-            node = node.left;
+    public static TreeNode getRightMostNode(TreeNode node, TreeNode curr) {
+        while (node.right != null && node.right != curr) {
+            node = node.right;
         }
+
+        return node;
     }
     public boolean isValidBST(TreeNode root) {
-        LinkedList<TreeNode> st = new LinkedList<>();
-
-        insertAllLeft(root, st);
-        long prev = -(long)1e13;
-        while(st.size() != 0) {
-            TreeNode rnode = st.removeFirst();
-            
-            if(prev >= rnode.val) {
-                return false;
+        TreeNode curr = root;
+        long prev = -(long) 1e13;
+        while(curr != null) {
+            TreeNode left = curr.left;
+            if(left == null) {
+                if(prev >= curr.val) {
+                    return false;
+                }
+                prev = curr.val;
+                
+                
+                curr = curr.right;
+            } else {
+                TreeNode rightMostNode = getRightMostNode(left, curr);
+                if(rightMostNode.right == null) {
+                    //Thread Creation Block
+                    rightMostNode. right = curr;
+                    curr = curr.left;
+                } else {
+                    //Thread Destroy Block
+                    rightMostNode.right = null;
+                    
+                    if(prev >= curr.val) {
+                        return false;
+                    }
+                    prev = curr.val;
+                    
+                    curr = curr.right;
+                }
             }
-            prev = rnode.val;
-            
-            insertAllLeft(rnode.right, st);
         }
         return true;
     }
