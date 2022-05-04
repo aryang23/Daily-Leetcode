aa303cd84cce648c6d9a884b423cf29366432be6
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-05-04 13:20:38 +0530

    Time: 3 ms (10.20%), Space: 45.4 MB (27.01%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -1,40 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    
-    public static TreeNode getRightMostNode(TreeNode node, TreeNode curr) {
-        while (node.right != null && node.right != curr) {
-            node = node.right;
+    private TreeNode getRightMostNode(TreeNode node, TreeNode curr) {
+            while (node.right != null && node.right != curr) {
+                node = node.right;
+            }
+
+            return node;
         }
-
-        return node;
-    }
-
-    
     public int kthSmallest(TreeNode root, int k) {
         int rv = -1;
         TreeNode curr = root;
-        while (curr != null) {
+        while(curr != null) {
             TreeNode left = curr.left;
-            if (left == null) {
-                if (--k == 0)
+            if(left == null) {
+                if(--k == 0) {
                     rv = curr.val;
+                }
                 curr = curr.right;
             } else {
                 TreeNode rightMostNode = getRightMostNode(left, curr);
-                if (rightMostNode.right == null) { // thread creation block
-                    rightMostNode.right = curr; // thread is created
-
+                if(rightMostNode.right == null) {
+                    //TCB
+                    rightMostNode.right = curr;
                     curr = curr.left;
-                } else { // thread destroy block
-                    rightMostNode.right = null; // thread is cut down
-
-                    if (--k == 0)
+                } else {
+                    //TDB
+                    rightMostNode.right = null;
+                    if(--k == 0) {
                         rv = curr.val;
-
+                    }
                     curr = curr.right;
                 }
             }
         }
-
         return rv;
     }
 }
