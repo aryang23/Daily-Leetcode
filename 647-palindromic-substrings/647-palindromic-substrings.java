3e9e52c802ba260147d35d37fb21a5934aaf5e30
Aryan Grover <aryangrover230402@gmail.com>
2022-05-22 09:02:55 +0530

    Time: 26 ms (20.89%), Space: 44.4 MB (24.84%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+        int len = 0;
+        for(int gap=0;gap<n;gap++)
+        {
+            for(int i=0, j=gap;j<n;j++, i++)
+            {
+                if(gap==0)
+                    dp[i][j] = true;
+                else if(gap==1 && s.charAt(i)==s.charAt(j))
+                    dp[i][j] = true;
+                else
+                    dp[i][j] = s.charAt(i)==s.charAt(j) && dp[i+1][j-1];
+                
+                if(dp[i][j]==true)
+                {
+                    count++;
+                    int currLen = j-i+1;
+                    if(currLen > len)
+                        len = currLen;
+                }
+            }
+        }
+        return count;
+    }
+}
