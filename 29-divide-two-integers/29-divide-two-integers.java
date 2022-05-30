765677292e830fb07433114ea85b969784654d6b
Aryan Grover <aryangrover230402@gmail.com>
2022-05-30 08:13:23 +0530

    Time: 1 ms (99.25%), Space: 42.2 MB (5.86%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -27,15 +27,9 @@ class Solution {
     }
     
     public int divide3(int dividend, int divisor) {
-
-    // Special case: overflow.
     if (dividend == Integer.MIN_VALUE && divisor == -1) {
         return Integer.MAX_VALUE;
     }
-
-    /* We need to convert both numbers to negatives
-     * for the reasons explained above.
-     * Also, we count the number of negatives signs. */
     int negatives = 2;
     if (dividend > 0) {
         negatives--;
@@ -45,18 +39,11 @@ class Solution {
         negatives--;
         divisor = -divisor;
     }
-
-    /* Count how many times the divisor has to be added
-     * to get the dividend. This is the quotient. */
     int quotient = 0;
     while (dividend - divisor <= 0) {
         quotient--;
         dividend -= divisor;
     }
-
-    /* If there was originally one negative sign, then
-     * the quotient remains negative. Otherwise, switch
-     * it to positive. */
     if (negatives != 1) {
         quotient = -quotient;
     }
@@ -71,9 +58,6 @@ class Solution {
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
-
-        /* We need to convert both numbers to negatives.
-         * Also, we count the number of negatives signs. */
         int negatives = 2;
         if (dividend > 0) {
             negatives--;
@@ -85,29 +69,16 @@ class Solution {
         }
 
         int quotient = 0;
-        /* Once the divisor is bigger than the current dividend,
-         * we can't fit any more copies of the divisor into it. */
         while (divisor >= dividend) {
-            /* We know it'll fit at least once as divivend >= divisor.
-             * Note: We use a negative powerOfTwo as it's possible we might have
-             * the case divide(INT_MIN, -1). */
             int powerOfTwo = -1;
             int value = divisor;
-            /* Check if double the current value is too big. If not, continue doubling.
-             * If it is too big, stop doubling and continue with the next step */
             while (value >= HALF_INT_MIN && value + value >= dividend) {
                 value += value;
                 powerOfTwo += powerOfTwo;
             }
-            // We have been able to subtract divisor another powerOfTwo times.
             quotient += powerOfTwo;
-            // Remove value so far so that we can continue the process with remainder.
             dividend -= value;
         }
-
-        /* If there was originally one negative sign, then
-         * the quotient remains negative. Otherwise, switch
-         * it to positive. */
         if (negatives != 1) {
             return -quotient;
         }
