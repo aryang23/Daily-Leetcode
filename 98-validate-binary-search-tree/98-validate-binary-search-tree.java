b83826eb0648b9578e2aa176870bd0e8e20d7372
Aryan Grover <aryangrover230402@gmail.com>
2022-06-10 01:33:06 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (61.67%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,28 +14,28 @@
  * }
  */
 class Solution {
-    public static void insertAllLeft(TreeNode node, LinkedList<TreeNode> st) {
-        while(node != null) {
-            st.addFirst(node);
-            node = node.left;
-        }
-    }
-
+    TreeNode prev = null;
     public boolean isValidBST(TreeNode root) {
-        LinkedList<TreeNode> st = new LinkedList<>();
-        insertAllLeft(root, st);
-        long prev = -(long)1e13;
-
-        while(st.size() != 0) {
-            TreeNode rNode = st.removeFirst();
-
-            if(prev >= rNode.val) {
-                return false;
-            }
-            prev = rNode.val;
-
-            insertAllLeft(rNode.right, st);
+        prev = null;
+        return helper(root);
+    }
+    
+    public boolean helper(TreeNode curr) {
+        if(curr == null) {
+            return true;
         }
-        return true;
+        
+        boolean la = helper(curr.left);
+        
+        //Work
+        if(prev != null && prev.val >= curr.val) {
+            return false;
+        }
+        //Update
+        prev = curr;
+        
+        boolean ra = helper(curr.right);
+        
+        return la && ra;
     }
 }
