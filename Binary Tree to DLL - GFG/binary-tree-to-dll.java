1b707b37a9d288b2220cb49303fa8c9909cd33d5
Aryan Grover <aryangrover230402@gmail.com>
2022-06-10 12:28:08 +0530

    Added solution - LeetHub

--- Binary Tree to DLL - GFG/binary-tree-to-dll.java
+++ Binary Tree to DLL - GFG/binary-tree-to-dll.java
@@ -149,44 +149,31 @@ class Node
 class Solution
 {
     //Function to convert binary tree to doubly linked list and return it.
-    private Node getRightMostNode(Node node, Node curr) {
-            while (node.right != null && node.right != curr) {
-                node = node.right;
-            }
-
-            return node;
-        }
+    Node prev;
     Node bToDLL(Node root)
     {
-	//  Your code here	
-	    Node dummy = new Node(-1);
-	    Node curr = root, prev = dummy;
-	    while(curr != null) {
-	        Node left = curr.left;
-	        if(left == null) {
-	            prev.right = curr;
-	            curr.left = prev;
-	            prev = prev.right;
-	            
-	            curr = curr.right;
-	        } else {
-	            Node rightMostNode = getRightMostNode(left, curr);
-	            if(rightMostNode.right == null) {
-	                rightMostNode.right = curr;
-	                curr = curr.left;
-	            } else {
-	                rightMostNode.right = null;
-	                prev.right = curr;
-	                curr.left = prev;
-	                prev = prev.right;
-	                
-	                curr = curr.right;
-	            }
-	        }
-	    }
-	    
-	    Node head = dummy.right;
-	    dummy.right = head.left = null;
-	    return head;
+	    Node dh = new Node(-1);
+        prev = dh;
+
+        helper(root);
+
+        //Circular
+        Node head = dh.right;
+        head.left = null;
+        return head;
+    }
+    public void helper(Node curr) {
+        if(curr == null) {
+            return;
+        }
+
+        helper(curr.left);
+
+        //Work
+        prev.right = curr;
+        curr.left = prev;
+        prev = curr;
+
+        helper(curr.right);
     }
 }
