1f9e21e1e63747ce502b71e7c82935dced0299d5
Aryan Grover <aryangrover230402@gmail.com>
2022-06-10 11:27:05 +0530

    Time: 3 ms (65.16%), Space: 47.9 MB (13.57%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -1,29 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    TreeNode big,small,prev;
+    TreeNode first;
+    TreeNode second;
+    TreeNode prev;
     public void recoverTree(TreeNode root) {
-        big=null;small=null;prev=null;
-        rec(root);
-        if(big!=null && small!=null){
-            int temp=big.val;
-            big.val=small.val;
-            small.val=temp;     
-        }
+        first = second = prev = null;
+        
+        helper(root);
+        
+        //Recover
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
     
-    private void rec(TreeNode root){
-        if(root==null) return ;
-        rec(root.left);
-        if(big!=null){
-            if(small==null && root.val>big.val) big=root;
-            else if(small!=null && root.val<small.val){
-                small=root;
-            }else if(small==null && root.val<prev.val){
-                small=root;
-            }
-        }else{
-            big=root;
+    public void helper(TreeNode curr) {
+        if(curr == null) {
+            return;
         }
-        prev=root;
-        rec(root.right);
+        helper(curr.left);
+        
+        //Work
+        if(prev!= null && prev.val > curr.val) {
+            if(first == null) {
+                //First Mistake
+                first = prev;
+                second = curr;
+            } else {
+                //Second Mistake
+                second = curr;
+            }
+        }
+        prev = curr;
+        
+        helper(curr.right);
     }
 }
