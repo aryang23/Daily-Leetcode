66aac835c02c30078262af0fa05fe173aacb5130
Aryan Grover <aryangrover230402@gmail.com>
2022-07-08 01:43:49 +0530

    Time: 6 ms (41.30%), Space: 125.8 MB (75.12%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,43 +1,27 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        
-        //Base Case
-        int contains = 0;
-        for(int i=0; i<n; i++) {
-            if(nums[i] == 1) {
-                contains++;
-                break;
-            }
-        }
-        if(contains == 0) {
-            return 1;
-        }
-        
-        //Replacing all negatives, zeroes
-        //And >n by 1
-        for(int i=0; i<n; i++) {
-            if((nums[i] <= 0) || (nums[i]>n)) {
-                nums[i] = 1;
-            }
-        }
-        for(int i=0; i<n; i++) {
-            int a = Math.abs(nums[i]);
-            if(a == n) {
-                nums[0] = - Math.abs(nums[0]);
-            } else {
-                nums[a] = - Math.abs(nums[a]);
+        //Put MAX in place of -ve
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] <= 0) {
+                nums[i] = Integer.MAX_VALUE;
             }
         }
         
-        for(int i=1; i<n; i++) {
-            if(nums[i] > 0) 
-                return i;
-        }
+        //Marking
+        for(int i=0; i<nums.length; i++) {
+            int val = Math.abs(nums[i]);
             
-        if(nums[0] > 0) {
-            return n;
+            if(val-1 >= 0 && val-1 < nums.length && nums[val-1] > 0) {
+                nums[val-1] = -nums[val-1];
+            }
         }
-        return n+1;
+        
+        //Final Ans -> First Unmarked spot
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] > 0) {
+                return i+1;
+            }
+        }
+        return nums.length + 1;
     }
 }
